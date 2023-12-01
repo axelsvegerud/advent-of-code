a28dfd29a04dab18c2fd203e193a6883433da28f
Axel Svegerud <axel.svegerud@gmail.com>
2023-12-01 11:54:44 +0100

    Setup

---
+++ AoC23/Days/Day01.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC23.Days
+{
+    class Day01
+    {
+        public static String Part01()
+        {
+            return "Part 1";
+        }
+
+        public static String Part02()
+        {
+            return "Part 2";
+        }
+    }
+}
---
+++ AoC23/Program.cs
@@ -0,0 +1,13 @@
+﻿using AoC23.Days;
+
+namespace AoC23
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Day01.Part01());
+            Console.WriteLine(Day01.Part02());
+        }
+    }
+}
