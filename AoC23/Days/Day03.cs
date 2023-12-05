464ef749a09f33ece023c16c9c8f30eae29d9f94
Axel Svegerud <axel.svegerud@gmail.com>
2023-12-05 23:44:22 +0100

    Day 3 2023 done

--- AoC23/Days/Day03.cs
+++ AoC23/Days/Day03.cs
@@ -5,7 +5,6 @@ namespace AoC23.Days
     class Day03
     {
         static string inputPath = $"{Environment.CurrentDirectory}\\Input\\Input_03.txt";
-        //static string inputPath = $"{Environment.CurrentDirectory}\\Input\\test.txt";
 
         public static int Part01()
         {
@@ -17,7 +16,7 @@ namespace AoC23.Days
             {
                 for (int j = 0; j < input[i].Length; j++)
                 {
-                    if (Char.IsDigit(input[i][j]) && adjacentToSymbol(input, i, j))
+                    if (char.IsDigit(input[i][j]) && adjacentToSymbol(input, i, j))
                     {
                         StringBuilder sb = new StringBuilder();
                         nbrs.Add(getNumber(input, i, j, sb));
@@ -27,9 +26,58 @@ namespace AoC23.Days
             return nbrs.Sum();
         }
 
-        public static string Part02()
+        public static int Part02()
         {
-            return "Part 2";
+            List<string> input = [.. File.ReadAllLines(inputPath)];
+
+            List<int> nbrs = [];
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (input[i][j] == '*')
+                    {
+                        int[] gearRetio = getNeighbors(input, i, j);
+                        if (gearRetio.Length == 2)
+                        {
+                            nbrs.Add(gearRetio[0] * gearRetio[1]);
+                        }
+                    }
+                }
+            }
+            return nbrs.Sum();
+        }
+
+        private static int[] getNeighbors(List<string> list, int row, int col)
+        {
+            List<int> ratio = new List<int>();
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int rowIndex = row + i;
+                        int colIndex = col + j;
+
+                        if (rowIndex >= 0 && rowIndex < list.Count && colIndex >= 0 && colIndex < list[rowIndex].Length)
+                        {
+                            if (char.IsDigit(list[rowIndex][colIndex]))
+                            {
+                                StringBuilder sb = new StringBuilder();
+                                ratio.Add(getNumber(list, rowIndex, colIndex, sb));
+                            }
+                        }
+                    }
+                }
+            }
+            return ratio.ToArray();
         }
 
         private static bool adjacentToSymbol(List<string> list, int row, int col)
@@ -44,7 +92,7 @@ namespace AoC23.Days
                         {
                             continue;
                         }
-                        if (!Char.IsDigit(list[i][j]) && list[i][j] != '.')
+                        if (!char.IsDigit(list[i][j]) && list[i][j] != '.')
                         {
                             return true;
                         }
@@ -58,7 +106,7 @@ namespace AoC23.Days
         {
             while (col != 0)
             {
-                if (Char.IsDigit(list[row][col - 1]))
+                if (char.IsDigit(list[row][col - 1]))
                 {
                     col = col - 1;
                 }
@@ -72,7 +120,7 @@ namespace AoC23.Days
 
             while (col < list[row].Length - 1)
             {
-                if (Char.IsDigit(list[row][col + 1]))
+                if (char.IsDigit(list[row][col + 1]))
                 {
                     col = col + 1;
                     sb.Append(list[row][col]);
@@ -83,33 +131,7 @@ namespace AoC23.Days
                     break;
                 }
             }
-
             return int.Parse(sb.ToString());
-        }
-
-        static void ExtendNumberSequence(string[] lines, int row, int col, StringBuilder sequence)
-        {
-            for (int i = row - 1; i <= row + 1; i++)
-            {
-                for (int j = col - 1; j <= col + 1; j++)
-                {
-                    if (i == row && j == col)
-                        continue;
-
-                    if (i >= 0 && i < lines.Length && j >= 0 && j < lines[i].Length)
-                    {
-                        char adjacentChar = lines[i][j];
-
-                        if (Char.IsDigit(adjacentChar))
-                        {
-                            sequence.Append(adjacentChar);
-                            // Mark the digit as visited to avoid duplicate counting
-                            lines[i] = lines[i].Substring(0, j) + ' ' + lines[i].Substring(j + 1);
-                            ExtendNumberSequence(lines, i, j, sequence);
-                        }
-                    }
-                }
-            }
         }
     }
 }
