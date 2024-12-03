51054542a0d5d8fe1c5969a8d2b54422bf7e2fe1
Axel Svegerud <axel.svegerud@gmail.com>
2024-12-03 20:08:23 +0100

    Day 3 Part 2 done🎄

--- AoC24/Days/day03.py
+++ AoC24/Days/day03.py
@@ -4,18 +4,35 @@ def solve_part1(data):
     sum_of_results = 0
 
     for mul in find_mul(data):
-        nums = re.findall(r"\d+", mul)
-        result = int(nums[0]) * int(nums[1])
-        sum_of_results += result
+        sum_of_results += evaluate_mul(mul)
 
     return sum_of_results
 
 def solve_part2(data):
-    # Implement solution for part 2
-    pass
+    sum_of_results = 0
+    disable = False
+    instructions = find_instructions(data)
+
+    for instruction in instructions:
+        if "don't" in instruction:
+            disable = True
+        elif "do" in instruction:
+            disable = False
+
+        if "mul" in instruction and not disable:
+            sum_of_results += evaluate_mul(instruction)
+
+    return sum_of_results
+
+def evaluate_mul(expression):
+    nums = re.findall(r"\d+", expression)
+    return int(nums[0]) * int(nums[1])
 
 def find_mul(data):
     return re.findall(r"mul\(\d+,\d+\)", data)
+
+def find_instructions(data):
+    return re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", data)
 
 if __name__ == "__main__":
     with open("../Inputs/day03.txt") as f:
